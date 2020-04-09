29dc738cced2215aa9f431ca9123eb866a1e4969
fregoco <814959152@qq.com>
2020-04-09 16:27:17 +0800

    Add two files.

---
+++ script.js
@@ -0,0 +1,19 @@
+// alert("Hello, javascript!");
+// function sayHello() {
+// 	console.log("hello, javascript!");
+// }
+
+// sayHello();
+
+// var sayBye = function() {
+// 	console.log("Bye!");
+// }
+
+// sayBye();
+
+function multiply(a, b) {
+	console.log(a, b);
+	return a * b;
+}
+
+multiply(5, 10);
